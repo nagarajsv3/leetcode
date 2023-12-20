283133dd9075282ba31cac9ac96021a65b9efeee
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2023-12-20 10:13:45 -0500

    Time: 16 ms (44.71%), Space: 45.1 MB (6.87%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,66 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        
+        if(s.isEmpty() || t.isEmpty() || (t.length() > s.length())){
+            return "";
+        }
+
+        if(s.equals(t)){
+            return t;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            char tch = t.charAt(i);
+            map.put(tch, map.getOrDefault(tch,0) + 1);        
+        }
+        int count = map.size();
+
+        int i = 0; //Pointer to release unwanted chars. increase the freq in map
+        int j = 0; //Pointer to acquire wanted chars. decrease the freq in map
+
+        int left = -1;
+        int right = -1;
+        int min = Integer.MAX_VALUE;
+        boolean found = false;
+
+        //Acquire Substring which has all chars
+        while( j < s.length()){
+            char sch = s.charAt(j);
+            j++;
+
+            if(map.containsKey(sch)){
+                map.put(sch, map.get(sch) - 1);
+                if(map.get(sch) == 0){
+                    count -- ; 
+                }
+
+                if(count > 0){
+                    continue;
+                }
+
+                //Shorten the substring which has all chars
+                while(i < s.length() && count == 0){
+                    found= true;
+                    char ich =s.charAt(i);    
+                    i++;
+
+                    if(map.containsKey(ich)){
+                        map.put(ich, map.get(ich)+1);
+                        if(map.get(ich) > 0){
+                            count++;
+                        }
+                    }
+                }
+
+                if(min > (j-i)){
+                    found =true;
+                    min = j-i;
+                    left = i-1;
+                    right = j-1;
+                }
+            }
+        }
+        return !found ? "": s.substring(left, right+1); 
+
+    }
+}
