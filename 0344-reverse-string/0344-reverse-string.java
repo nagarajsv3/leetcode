9e5c261d0f1cd58174635494c8125c24fd036d33
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2023-12-20 10:44:23 -0500

    Time: 0 ms (100.00%), Space: 49.1 MB (98.05%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        for(int i=0, j=s.length - 1; i< (s.length)/2 ; i++, j--){
+            char fc = s[i];
+            s[i] = s[j];
+            s[j] = fc;
+        }
+        
+    }
+}
