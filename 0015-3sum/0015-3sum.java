9c1e139bbf0874518174ea7b1b6c0e4efa08fbd8
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2023-12-21 21:20:38 -0500

    Time: 34 ms (46.47%), Space: 52.7 MB (5.64%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);             // O(N log N )
-        for(int i=0; i< nums.length ; i++){     // O(N log N  + N)
-            if(i==0 || i>0 && nums[i] !=nums[i-1]){
-                int j=i+1 ;
-                int k=nums.length-1;
-                while(j<nums.length && k<nums.length && j < k){ // O(N log N + N^2)
-                    if(nums[i] + nums[j] + nums[k] == 0){
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]); temp.add(nums[j]); temp.add(nums[k]);
-                        list.add(temp);
-                        do{
-                            j++;
-                        }while(j<nums.length && nums[j] == nums[j-1]);
-                        do{
-                            k--;
-                        }while(k>0 && nums[k] == nums[k+1]);
-                    }else if(nums[i] + nums[j] + nums[k] > 0){
-                        k--;
-                    }else if(nums[i] + nums[j] + nums[k] < 0){
+        //Sort Array nums
+        //Fix i ; move j from i+1 till < k ; Move k from len-1 till >j
+        
+        List<List<Integer>> triplets = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for(int i=0; i<nums.length-2 ; i++){
+            if(i!=0 && nums[i] == nums[i-1]){
+                continue;
+                //To avoid duplicate triplet
+            }
+            int j=i+1; 
+            int k=nums.length-1;
+            while(j<k){
+                if(nums[i]+nums[j]+nums[k] == 0){
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[j]);
+                    triplet.add(nums[k]);
+                    triplets.add(triplet);
+                    
+                    do{
                         j++;
-                    }
+                    }while(j<k && nums[j]==nums[j-1]);
+                    
+                    do{
+                        k--;
+                    }while(k>j && nums[k] == nums[k+1]);
+                    
+                }else if(nums[i]+nums[j]+nums[k] > 0){
+                    k--;
+                }else if(nums[i]+nums[j]+nums[k] < 0){
+                    j++;
                 }
+                
             }
         }
-        return list;
+        
+        return triplets;
     }
-
-    //TC O(N^2)
-    //SC O(U) Unique triplets
 }
