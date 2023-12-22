c27a77ed4a0d2c510bf09d753e18a5d8258ea405
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2023-12-22 15:10:44 -0500

    Time: 0 ms (100.00%), Space: 42.3 MB (6.08%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -17,38 +17,32 @@ class Solution {
         ListNode res = null;
         
         if(list1.val <= list2.val){
-            res = list1;//1
-            
+            res = list1;
         }else{
-
-            ListNode swp = list1;
-                list1 = list2;
-                list2 = swp;
+            ListNode t=list1;
+            list1 = list2;
+            list2 = t;
             res = list1;
         }
         
-        while(list1 !=null && list2 !=null){
+        while(list1!=null && list2!=null){
+            
             ListNode temp = new ListNode();
-            while(list1!=null && list1.val <= list2.val){
-                temp = list1; // 1
+            while(list1!=null && list1.val<=list2.val){
+                temp = list1;
                 list1 = list1.next;
             }
             temp.next = list2;
             
             if(list1!=null){
-                ListNode swp = list1;
-                list1 = list2;
-                list2 = swp;
-                            
-            }
+                ListNode t2= list1;
+                list1 = list2 ;
+                list2 = t2;
+            }            
+            
         }
         return res;
     }
     
-    void swap(ListNode list1, ListNode list2){
-        ListNode temp = list1;
-        list1 = list2;
-        list2 = temp;
-    }
     
 }
