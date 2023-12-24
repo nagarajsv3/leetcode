e4a322885a63bfbc591b29c851e6b152e4c99bd2
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2023-12-24 18:42:44 -0500

    Time: 4 ms (78.67%), Space: 41.7 MB (24.15%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.java
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -1,56 +1,56 @@
 class Solution {
-    
-    private Map<Character, List<Character>> reverseAdjList = new HashMap<>();
-    private Map<Character, Boolean> seen = new HashMap<>();
-    private StringBuilder output = new StringBuilder();
-    
     public String alienOrder(String[] words) {
-        
-        // Step 0: Put all unique letters into reverseAdjList as keys.
-        for (String word : words) {
-            for (char c : word.toCharArray()) {
-                reverseAdjList.putIfAbsent(c, new ArrayList<>());
-            }
+    
+    // Step 0: Create data structures and find all unique letters.
+    Map<Character, List<Character>> adjList = new HashMap<>();
+    Map<Character, Integer> counts = new HashMap<>();
+    for (String word : words) {
+        for (char c : word.toCharArray()) {
+            counts.put(c, 0);
+            adjList.put(c, new ArrayList<>());
         }
-        
-        // Step 1: Find all edges and add reverse edges to reverseAdjList.
-        for (int i = 0; i < words.length - 1; i++) {
-            String word1 = words[i];
-            String word2 = words[i + 1];
-            // Check that word2 is not a prefix of word1.
-            if (word1.length() > word2.length() && word1.startsWith(word2)) {
-                return "";
-            }
-            // Find the first non match and insert the corresponding relation.
-            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
-                if (word1.charAt(j) != word2.charAt(j)) {
-                    reverseAdjList.get(word2.charAt(j)).add(word1.charAt(j));
-                    break;
-                }
-            }
-        }
-        
-        // Step 2: DFS to build up the output list.
-        for (Character c : reverseAdjList.keySet()) {
-            boolean result = dfs(c);
-            if (!result) return "";
-        }
-        
-        return output.toString();
     }
     
-    // Return true iff no cycles detected.
-    private boolean dfs(Character c) {
-        if (seen.containsKey(c)) {
-            return seen.get(c); // If this node was grey (false), a cycle was detected.
+    // Step 1: Find all edges.
+    for (int i = 0; i < words.length - 1; i++) {
+        String word1 = words[i];
+        String word2 = words[i + 1];
+        // Check that word2 is not a prefix of word1.
+        if (word1.length() > word2.length() && word1.startsWith(word2)) {
+            return "";
         }
-        seen.put(c, false);
-        for (Character next : reverseAdjList.get(c)) {
-            boolean result = dfs(next);
-            if (!result) return false;
+        // Find the first non match and insert the corresponding relation.
+        for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
+            if (word1.charAt(j) != word2.charAt(j)) {
+                adjList.get(word1.charAt(j)).add(word2.charAt(j));
+                counts.put(word2.charAt(j), counts.get(word2.charAt(j)) + 1);
+                break;
+            }
         }
-        seen.put(c, true);
-        output.append(c);
-        return true;
-    }    
+    }
+    
+    // Step 2: Breadth-first search.
+    StringBuilder sb = new StringBuilder();
+    Queue<Character> queue = new LinkedList<>();
+    for (Character c : counts.keySet()) {
+        if (counts.get(c).equals(0)) {
+            queue.add(c);
+        }
+    }
+    while (!queue.isEmpty()) {
+        Character c = queue.remove();
+        sb.append(c);
+        for (Character next : adjList.get(c)) {
+            counts.put(next, counts.get(next) - 1);
+            if (counts.get(next).equals(0)) {
+                queue.add(next);
+            }
+        }
+    }
+    
+    if (sb.length() < counts.size()) {
+        return "";
+    }
+    return sb.toString();
+}
 }
