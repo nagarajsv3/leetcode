41b934c20c7b6929463718377f9867ae7f852aea
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2023-12-24 18:39:04 -0500

    Time: 4 ms (78.67%), Space: 41.8 MB (15.09%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.java
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -1,104 +1,56 @@
 class Solution {
+    
+    private Map<Character, List<Character>> reverseAdjList = new HashMap<>();
+    private Map<Character, Boolean> seen = new HashMap<>();
+    private StringBuilder output = new StringBuilder();
+    
     public String alienOrder(String[] words) {
-        //Create AdjList
-        int V = 26;
-        List<List<Integer>> adjlist = new ArrayList<>();
-        for(int i=0;i<V;i++){
-            adjlist.add(new ArrayList<>());
-        }
         
-        Set<Character> set = new HashSet<>();
-        
-        if(words.length==1){
-            if(words[0].equals("aba")){
-                return "ab";
+        // Step 0: Put all unique letters into reverseAdjList as keys.
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                reverseAdjList.putIfAbsent(c, new ArrayList<>());
             }
-            return words[0];
         }
-
-        for(int i=0; i<words.length-1 ; i++){
-            
-            //COmpare 1st , 2nd
-            int min = Math.min(words[i].length(), words[i+1].length());
-            for(int j=0;j<min;j++){
-                String fir = words[i];
-                String sec = words[i+1];
-                
-                for(int l=0;l<words[i].length();l++){
-                    set.add(words[i].charAt(l));
-                }
-                
-                for(int l=0;l<words[i+1].length();l++){
-                    set.add(words[i+1].charAt(l));
-                }
-                
-                if(fir.startsWith(sec) && fir.length() > sec.length()){
-                    return "";
-                }
-                
-                if (fir.charAt(j)!=sec.charAt(j)){
-                    //(u, v) fir.char sec.char
-                    int u = fir.charAt(j) - 'a';
-                    set.add(fir.charAt(j));
-                    int v = sec.charAt(j) - 'a';
-                    set.add(sec.charAt(j));
-                    adjlist.get(u).add(v);
+        
+        // Step 1: Find all edges and add reverse edges to reverseAdjList.
+        for (int i = 0; i < words.length - 1; i++) {
+            String word1 = words[i];
+            String word2 = words[i + 1];
+            // Check that word2 is not a prefix of word1.
+            if (word1.length() > word2.length() && word1.startsWith(word2)) {
+                return "";
+            }
+            // Find the first non match and insert the corresponding relation.
+            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
+                if (word1.charAt(j) != word2.charAt(j)) {
+                    reverseAdjList.get(word2.charAt(j)).add(word1.charAt(j));
                     break;
                 }
             }
-        }    
- 
-        //createinputdegreearray(adjlist);
-        int[] inarr = new int[V];
-        for(int i=0;i<V;i++){
-            for(int each : adjlist.get(i)){
-                inarr[each] = inarr[each]+1;
-            }
-        }
-                
-        //push0guystoqueue();
-        Queue<Integer> queue = new ArrayDeque<>();
-        for(int i=0; i<V;i++){
-            if(inarr[i] ==0 && presentinset(i, set)){
-                queue.offer(i);
-            }
         }
         
-        char[] output = new char[V];
-        int cnt = 0;
-        int m=0;
-        
-        while(!queue.isEmpty()){
-            int node = queue.poll();
-            output[m++] = (char)(node + 'a');
-            cnt++;
-                            
-            
-            for(int each : adjlist.get(node)){
-                if(inarr[each] != 0){
-                    inarr[each] = inarr[each]-1;
-                    if(inarr[each] ==0){
-                        queue.offer(each);
-                    }
-                }
-            }
+        // Step 2: DFS to build up the output list.
+        for (Character c : reverseAdjList.keySet()) {
+            boolean result = dfs(c);
+            if (!result) return "";
         }
         
-        
-        String out = "";
-        for(int n=0;n<set.size();n++){
-            out = out+output[n];
-        }
-        return cnt==set.size()  ? out : "";
+        return output.toString();
     }
     
-    boolean presentinset(int i, Set<Character> set){
-        char ch = (char)(i+'a');
-        
-        return set.contains(ch);
-    }
-    
-
-    
-    
+    // Return true iff no cycles detected.
+    private boolean dfs(Character c) {
+        if (seen.containsKey(c)) {
+            return seen.get(c); // If this node was grey (false), a cycle was detected.
+        }
+        seen.put(c, false);
+        for (Character next : reverseAdjList.get(c)) {
+            boolean result = dfs(next);
+            if (!result) return false;
+        }
+        seen.put(c, true);
+        output.append(c);
+        return true;
+    }    
 }
