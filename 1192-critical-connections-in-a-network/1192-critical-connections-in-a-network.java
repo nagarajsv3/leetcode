3be51ba3ad630a9652e7db8c15ec5fd3539b693a
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2023-12-26 21:27:15 -0500

    Time: 85 ms (63.56%), Space: 115.4 MB (49.01%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,33 +1,34 @@
 class Solution {
-    int timer=0;
+    int timer = 0;
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
         boolean[] visited = new boolean[n];
-        int[] discovery = new int[n];
+        int[] ins = new int[n];
         int[] low = new int[n];
         
         List<List<Integer>> adjlist = new ArrayList<>();
-        for(int i=0; i<n;i++){
+        for(int i=0; i<n; i++){
             adjlist.add(new ArrayList<>());
         }
-        for(int i=0;i<connections.size();i++){
-            int u = connections.get(i).get(0);
-            int v = connections.get(i).get(1);
+        
+        for(List<Integer> conn : connections){
+            int u = conn.get(0);
+            int v = conn.get(1);
             adjlist.get(u).add(v);
             adjlist.get(v).add(u);
         }
         
         List<List<Integer>> bridges = new ArrayList<>();
-        dfs(0, -1, visited, adjlist, discovery , low, bridges);
         
+        dfs(0, -1, visited, adjlist, ins, low, bridges);
         return bridges;
+        
     }
     
-    void dfs(int node, int parent, boolean[] visited, List<List<Integer>> adjlist, int[] discovery, int[] low, List<List<Integer>> bridges){
-        
+    void dfs(int node , int parent, boolean[] visited, List<List<Integer>> adjlist, int[] ins, int[] low, List<List<Integer>> bridges){
         visited[node] = true;
-        timer  = timer+1;
-        discovery[node] = timer;
+        ins[node] = timer;
         low[node] = timer;
+        timer= timer+1;
         
         for(int neigh : adjlist.get(node)){
             if(neigh == parent){
@@ -35,14 +36,19 @@ class Solution {
             }
             
             if(visited[neigh] == false){
-                dfs(neigh, node , visited, adjlist, discovery, low, bridges);
-                low[node] = Math.min(low[neigh], low[node]);
-                if(low[neigh] > discovery[node]){
-                    ArrayList<Integer> list = new ArrayList<>(); list.add(neigh); list.add(node);
-                    bridges.add(list);
+                dfs(neigh , node, visited, adjlist, ins, low, bridges);
+                low[node] = Math.min(low[node], low[neigh]);
+                //bridge  ancestor -- node -- neigh
+                if(low[neigh] > ins[node] ){
+                    //Its a bridge
+                    List<Integer> bridge = new ArrayList<>();
+                    bridge.add(node);
+                    bridge.add(neigh);
+                    bridges.add(bridge);
                 }
+            
             }else{
-                low[node] = Math.min(low[neigh], low[node]);
+                low[node] = Math.min(low[node], low[neigh]);
             }
         }
         
