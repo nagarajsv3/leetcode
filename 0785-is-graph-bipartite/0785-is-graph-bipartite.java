4b85ac8b15153ecfca36584e455ced321db493f4
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-03 10:45:31 -0500

    Time: 2 ms (44.13%), Space: 44.6 MB (32.91%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,36 +1,34 @@
 class Solution {
-    
-    //Using BFS
     public boolean isBipartite(int[][] graph) {
-        int len = graph.length;
-        
-        int[] color = new int[len];
+        int verlen =  graph.length;
+        int[] color = new int[verlen] ;
         Arrays.fill(color, -1);
         
         Queue<Integer> queue = new ArrayDeque<>();
-        for(int i=0;i<len; i++){
-            if(color[i] ==-1){
+        
+        for(int i=0;i<verlen;i++){
+            if(color[i] == -1){
                 queue.offer(i);
                 color[i] = 0;
                 
-                while(!queue.isEmpty()){
-                    int node = queue.poll();
-                    
-                    int[] neighbours = graph[node];
-                    
-                    for(int neigh : neighbours){
-                        if(color[neigh] == -1){
-                            queue.offer(neigh);
-                            color[neigh] = 1 - color[node];
-                        }else{
-                            if(color[neigh] == color[node]){
-                                return false;
-                            }
-                        }
+                while(! queue.isEmpty()){
+                int node = queue.poll();
+
+                //color its neighbours
+                for(int neigh : graph[node]){
+                    if(color[neigh] == -1){
+                        queue.offer(neigh);
+                        color[neigh] = 1 - color[node];
+                    }else if(color[neigh] == color[node]){
+                        return false;
                     }
                 }
+        }
             }
         }
+        
+        
+        
         return true;
     }
 }
