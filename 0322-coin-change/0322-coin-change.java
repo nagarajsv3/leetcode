85db0e6c1f355ab2e9cada3302834926564887b4
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-02 21:08:25 -0500

    Time: 24 ms (34.18%), Space: 43.8 MB (46.79%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,33 +1,32 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        
+        //Edge Case amount 0
+        if(amount ==0 ){ return 0;}
+        
+        //Edge Case amount less than 0
+        if(amount < 0 ){ return -1;}
 
-        if(amount <0){
-            return -1;
-        }
-
-        if(amount ==0){
-            return 0;
-        }
-
-        int[] dp = new int[amount+1];
+        //amount - 3
+        //coins [2]
+        //dp [0, 4, 4, 4] 
+        
+        int[] dp = new int[amount+1];//
         Arrays.fill(dp, amount+1);
-        dp[0] = 0;
-
-        Arrays.sort(coins);
-
-        //Iterate through Money & populated DP table with best possible combination of coins
-        for(int money = 1 ; money <= amount ; money++){
-
+        dp[0]=0;
+        
+        Arrays.sort(coins); //3
+        
+        for(int money = 1 ; money<=amount ; money++){            
             for(int coin : coins){
                 if(money-coin >=0){
                     dp[money] = Math.min(dp[money], dp[money-coin] + 1);
                 }else{
                     break;
-                }
+                } 
             }
-
         }
-
-        return (dp[amount] == amount+1) ? -1 : dp[amount];
+        
+        return dp[amount] == amount+1 ? -1 : dp[amount];
     }
 }
