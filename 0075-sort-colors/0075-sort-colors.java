4cfc9335e4db2635a7cd7553231920d94e2b2bc1
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-02 21:40:57 -0500

    Time: 0 ms (100.00%), Space: 42 MB (6.50%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,54 +1,27 @@
 class Solution {
-    
-    //Dutch National Flag 3 Pointer Approach low, mid, high
-    public void sortColors(int[] nums){
-        int low = 0;
-        int mid = 0;
-        int high = nums.length-1;
-
-        while(mid <= high){
-            if(nums[mid] == 0){
-                swap(nums, low, mid);
-                low++;
-                mid++;
-            }else if(nums[mid]==1){
-                mid++;
-            }else if(nums[mid] ==2){
-                swap(nums , high, mid);
-                high--;
+    public void sortColors(int[] nums) {
+        int cnt0 = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int num : nums){
+            if(num==0){
+                cnt0++;
+            }else if(num==1){
+                cnt1++;
+            }else if(num==2){
+                cnt2++;
             }
         }
+        for(int i=0;i<cnt0;i++){
+            nums[i] = 0;
+        }
+        
+        for(int i=cnt0;i<(cnt0+cnt1);i++){
+            nums[i] = 1;
+        }
+        
+        for(int i=cnt0+cnt1 ; i< nums.length ; i++){
+            nums[i] = 2;
+        }
     }
-    //TC O(N)
-    //SC O(1)
-
-    void swap(int[] nums, int fir , int sec){
-        int temp = nums[fir];
-        nums[fir] = nums[sec];
-        nums[sec] = temp;
-    }
-
-    //Counting Sort
-    /* 
-    public void sortColors(int[] nums) {
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            freqMap.put(nums[i], freqMap.getOrDefault(nums[i], 0)+1);
-        }
-
-        int k=0;
-        for(; freqMap.containsKey(0) && k<freqMap.get(0) ; k++){
-            nums[k] = 0;
-        }
-
-        for(int l=0; freqMap.containsKey(1) && l<freqMap.get(1) ; l++, k++){
-            nums[k] = 1;
-        }
-
-        for(int m=0; freqMap.containsKey(2) && m<freqMap.get(2) ; m++, k++){
-            nums[k] = 2;
-        }
-    }*/
-    //TC - O(N)
-    //SC - O(N) 
 }
