a4b31c0dac4c3bf2e4eb27ef24807e2d90b28141
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-03 07:48:00 -0500

    Time: 43 ms (41.55%), Space: 52 MB (17.63%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,44 +1,52 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
         List<List<Integer>> finallist = new ArrayList<>();
         
+        //Sort Array - O(n logn)
+        Arrays.sort(nums);
         
-        for(int i=0; i<nums.length-2 ; i++){
-            if(i!=0 && nums[i] == nums[i-1]){
-                continue;
-                //To avoid duplicate triplet
-            }
-        
+        int i=0;
+        while(i<nums.length-2){
+            
+            
             int j=i+1;
             int k=nums.length-1;
             while(j<k){
-                if(nums[i]+nums[j]+nums[k]==0){
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
-                    finallist.add(list);
-                    
-                    //0 2 -1 -1 -2 -2
-                    do{
-                        k--;    
-                    }while(k>j && nums[k]==nums[k+1]);
-                    
+                if(nums[i]+nums[k]+nums[j] == 0){
+                    captureTriplet(nums[i], nums[j], nums[k], finallist);
                     do{
                         j++;
-                    }while(k>j && nums[j] == nums[j-1] );
-                }else if(nums[i]+ nums[j]+nums[k] >0){
+                    }while(j<k && nums[j]==nums[j-1]);
+                    do{
                         k--;
+                    }while(j<k && nums[k]==nums[k+1]);
                     
-                }else if(nums[i]+ nums[j]+nums[k] <0){
-                        j++;    
+                }else if(nums[i]+nums[k]+nums[j] > 0){
+                    do{
+                        k--;
+                    }while(j<k && nums[k]==nums[k+1]);
+                }else if(nums[i]+nums[k]+nums[j] < 0){
+                    do{
+                        j++;
+                    }while(j<k && nums[j]==nums[j-1]);
+                }
 
-                }   
             }
             
+            do{
+                i++;    
+            }while(i<nums.length-2 && nums[i] == nums[i-1]);
+            
         }
+        
         return finallist;
+    }
+    
+    void captureTriplet(int fnum, int snum, int tnum, List<List<Integer>> finallist){
+        List<Integer> list = new ArrayList<>();
+        list.add(fnum);
+        list.add(snum);
+        list.add(tnum);
+        finallist.add(list);
     }
 }
