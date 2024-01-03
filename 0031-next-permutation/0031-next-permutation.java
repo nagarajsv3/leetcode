1a7dacf403220185c10268b94e97ba66d46e0817
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-02 21:58:33 -0500

    Time: 0 ms (100.00%), Space: 42.9 MB (15.55%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,41 +1,42 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int len = nums.length-1;
-        int leastsigidx = len-1;
         
-        //1 2 4 9 8 7 6 5 3 -> value 4; pos= 2 -leastsigidx = 2 
-        while( leastsigidx>=0  && nums[leastsigidx] >= nums[leastsigidx + 1]){
+        int leastsigidx = nums.length-2;
+        
+        //1 2 4 9 8 7 6 5 3
+        while(leastsigidx >=0 && nums[leastsigidx] >= nums[leastsigidx+1]){
             leastsigidx--;
         }
         
-        if(leastsigidx >=0){
-            int bestidx = len;
-            while(bestidx>=0 && nums[bestidx] <= nums[leastsigidx]){
-                bestidx--;
-            }
-            //best digit idx value 5; pos= 7 -bestidx = 7 
-            
-            if(leastsigidx >=0 && bestidx >=0){
-                swap(nums, leastsigidx, bestidx);//swap 2th pos & 7th pos
+        if(leastsigidx >=0 ){
+            //then only perform swap with best digit
+            int bestdigitidx=nums.length-1;
+            while(bestdigitidx>=0 && nums[leastsigidx] >= nums[bestdigitidx]){
+                bestdigitidx--;
             }
             
+            if(bestdigitidx>=0){
+                swap(nums, leastsigidx, bestdigitidx);
+            }
         }
         
-        reverse(nums, Math.max(0, leastsigidx+1), len); //swap 3rd pos to last pos to get best next permutation value
+        //reverse 
+        int reversestarting = Math.max(0, leastsigidx+1);
+        reverse(nums, reversestarting, nums.length-1);
         
     }
     
+    void swap(int[] nums, int left, int right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+    
     void reverse(int[] nums, int left, int right){
-        while(left<=right){
+        while(left<right){
             swap(nums, left, right);
             left++;
             right--;
         }
-    }
-    
-    void swap(int[] nums, int fir, int sec){
-        int temp = nums[fir];
-        nums[fir] = nums[sec];
-        nums[sec] = temp;
     }
 }
