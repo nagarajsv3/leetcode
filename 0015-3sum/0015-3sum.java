bc62d8511f7ae824daca32eb23cac8a84db76d70
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-03 00:39:21 -0500

    Time: 50 ms (38.77%), Space: 52.9 MB (6.43%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,43 +1,44 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        //Sort Array nums
-        //Fix i ; move j from i+1 till < k ; Move k from len-1 till >j
-        
-        List<List<Integer>> triplets = new ArrayList<>();
         Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        List<List<Integer>> finallist = new ArrayList<>();
+        
         
         for(int i=0; i<nums.length-2 ; i++){
             if(i!=0 && nums[i] == nums[i-1]){
                 continue;
                 //To avoid duplicate triplet
             }
-            int j=i+1; 
+        
+            int j=i+1;
             int k=nums.length-1;
             while(j<k){
-                if(nums[i]+nums[j]+nums[k] == 0){
-                    List<Integer> triplet = new ArrayList<>();
-                    triplet.add(nums[i]);
-                    triplet.add(nums[j]);
-                    triplet.add(nums[k]);
-                    triplets.add(triplet);
+                if(nums[i]+nums[j]+nums[k]==0){
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    finallist.add(list);
+                    
+                    //0 2 -1 -1 -2 -2
+                    do{
+                        k--;    
+                    }while(k>j && nums[k]==nums[k+1]);
                     
                     do{
                         j++;
-                    }while(j<k && nums[j]==nums[j-1]);
-                    
-                    do{
+                    }while(k>j && nums[j] == nums[j-1] );
+                }else if(nums[i]+ nums[j]+nums[k] >0){
                         k--;
-                    }while(k>j && nums[k] == nums[k+1]);
                     
-                }else if(nums[i]+nums[j]+nums[k] > 0){
-                    k--;
-                }else if(nums[i]+nums[j]+nums[k] < 0){
-                    j++;
-                }
-                
+                }else if(nums[i]+ nums[j]+nums[k] <0){
+                        j++;    
+
+                }   
             }
+            
         }
-        
-        return triplets;
+        return finallist;
     }
 }
