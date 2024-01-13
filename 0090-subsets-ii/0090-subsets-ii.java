ba2974da354aa18157f8945ea25ee68dc0620134
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-13 11:08:52 -0500

    Time: 1 ms (99.82%), Space: 43.4 MB (82.53%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,5 +1,7 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        //TC - O(y+x) where y is nlogn  ; x is 2^n
+        //SC - O(x) where x is 2^n
         Arrays.sort(nums);
         List<List<Integer>> subsets = new ArrayList<>();
         subsetBacktrack(0, nums, new ArrayList<Integer>(), subsets);
@@ -10,6 +12,7 @@ class Solution {
         subsets.add(new ArrayList<>(subset));
         
         for(int i=index ; i<nums.length; i++){
+            //Condition tp remove duplicates
             if(i!=index && nums[i]== nums[i-1]){
                 continue;
             }
