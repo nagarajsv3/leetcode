e367fb96aca02ceaf7638ce02ee8146bd0eb6e8f
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-20 23:51:54 -0500

    Time: 0 ms (100.00%), Space: 42.3 MB (46.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -24,9 +24,7 @@ class Solution {
         
         ListNode smallest = null;
         while(list1!=null && list2 !=null){
-            System.out.println("Outer : list1="+list1.val+" list2="+list2.val);
             while(list1!=null && list1.val <=list2.val){
-                System.out.println("Inner : list1="+list1.val+" list2="+list2.val);
                 smallest = list1;
                 list1 = list1.next;
             }    
