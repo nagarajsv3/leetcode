ed166ea97c5d58174dcd37b3bf782247e07d97c7
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-21 16:05:55 -0500

    Time: 69 ms (47.80%), Space: 55.2 MB (18.97%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,74 +1,52 @@
 class Solution {
-
+    int count = 0;
     public int reversePairs(int[] nums) {
-        int count = mergeSort(nums, 0, nums.length-1);//1 3 2 3 1 //0, 4
+        mergesort(0, nums.length-1 , nums);
+        System.out.println(Arrays.toString(nums));
         return count;
     }
-
-    int mergeSort(int[] nums, int begin, int end){ //{1,3,2,3,1} //0,4
-        int count = 0;
+    
+    void mergesort(int begin, int end , int[] nums){
         if(end-begin<=0){
-             return count;
+            return;
         }
-
-        int mid = (begin + end)/2;//2
-        count += mergeSort(nums, begin, mid);//0, 2 // 0, 1
-        count += mergeSort(nums, mid+1, end);
-        count += countReversePairs(nums, begin, mid+1, end);
-        mergeBack(nums, begin, mid+1, end);
-        return count;
+        int mid = (begin+end)/2;
+        mergesort(begin, mid, nums);
+        mergesort(mid+1, end, nums);
+        countinversions(begin, mid+1, end, nums);
+        mergeback(begin, mid+1, end, nums);
     }
-
-    int countReversePairs(int[] nums, int left, int right, int end){
-        int count =0 ;
+    
+    void countinversions(int left, int right, int end, int[] nums){
         int leftend = right-1;
         int j=right;
-        for(int i=left; i<=leftend; i++){
-            while (j<=end && ((long)nums[i]  > 2 * (long)nums[j])){    
+        for(int i=left;i<=leftend; i++){
+            while(j<=end && (((long)nums[i]) > ((long)2*nums[j]))){
                 j++;
             }
-            count = count + (j - right);
+            count += (j-right);   
         }
-        return count;
     }
-
-    void mergeBack(int[] nums, int leftOri, int rightOri, int end){
-        int left = leftOri;
-        int right = rightOri;
-        
+    
+    void mergeback(int left, int right, int end, int[] nums){
         int leftend = right-1;
-
+        int i = left;
+        int j= right;
         int[] temp = new int[end-left+1];
         int k=0;
-        while(left<=leftend && right<=end){
-            if(nums[left] > nums[right]){
-                temp[k] = nums[right];
-                k++;
-                right++;
-            }else{
-                temp[k] = nums[left];
-                k++;
-                left++;
-            }
+        while(i<=leftend && j<=end){
+            temp[k++] = nums[i] < nums[j] ? nums[i++] : nums[j++];
         }
-
-        while(left<=leftend && right>end){
-            temp[k] = nums[left];
-            k++;
-            left++;
+        
+        while(i<=leftend && j>end){
+            temp[k++] = nums[i++];
         }
-
-        while(left>leftend && right<=end){
-            temp[k] = nums[right];
-            k++;
-            right++;
+        while(i>leftend && j<=end){
+            temp[k++] = nums[j++];
         }
-
-
-        int m=0;
-        for(int n=leftOri; n<=end;){
+        
+        for(int m=0, n=left ;m<temp.length;){
             nums[n++] = temp[m++];
         }
-
     }
 }
