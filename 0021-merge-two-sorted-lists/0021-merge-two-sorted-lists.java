891cda732cc2b1e8fbec3f9d6814b46f1470c2a4
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-20 23:51:29 -0500

    Time: 23 ms (13.10%), Space: 44.8 MB (7.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,40 +9,34 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {//1 2 4   //1 3 4   1
-        if(list1==null && list2==null){ return null;}
-        if(list1==null){ return list2;}
-        if(list2==null){ return list1;}
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         
-        ListNode res = null;
+        if(list1 == null && list2==null) return null;
+        if(list1 ==null && list2 !=null ) return list2;
+        if(list1 !=null && list2 == null) return list1;
         
-        if(list1.val <= list2.val){
-            res = list1;
-        }else{
-            ListNode t=list1;
-            list1 = list2;
-            list2 = t;
-            res = list1;
+        if(list1.val > list2.val){
+                ListNode t = list1;
+                list1 = list2;
+                list2 = t;
         }
+        ListNode ref = list1;
         
-        while(list1!=null && list2!=null){
-            
-            ListNode temp = new ListNode();
-            while(list1!=null && list1.val<=list2.val){
-                temp = list1;
+        ListNode smallest = null;
+        while(list1!=null && list2 !=null){
+            System.out.println("Outer : list1="+list1.val+" list2="+list2.val);
+            while(list1!=null && list1.val <=list2.val){
+                System.out.println("Inner : list1="+list1.val+" list2="+list2.val);
+                smallest = list1;
                 list1 = list1.next;
-            }
-            temp.next = list2;
-            
+            }    
+            smallest.next = list2;
             if(list1!=null){
-                ListNode t2= list1;
-                list1 = list2 ;
-                list2 = t2;
-            }            
-            
+                ListNode t = list1;
+                list1 = list2;
+                list2 = t;
+            }
         }
-        return res;
+        return ref;
     }
-    
-    
 }
