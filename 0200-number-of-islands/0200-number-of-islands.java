aa87d1f9a4ff5033c25025d1717ac5d90375abe9
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-21 20:07:26 -0500

    Time: 5 ms (34.41%), Space: 48.8 MB (63.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,61 +1,83 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
-
-        int counter=0;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j]=='1' && visited[i][j] == false){
-                    System.out.println("Starting from "+i+ " "+j);                
-                    bfs(i, j, visited, grid);
-                    counter++;
+        
+        int cnt = 0;
+        
+        for(int i=0; i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && visited[i][j]==false){
+                    bfs(i,j, visited, grid, m, n);
+                    cnt++;
                 }
             }
         }
-        return counter;
+        return cnt;
     }
-
-    void bfs(int row, int col, boolean[][] visited, char[][] grid){
-        Queue<Pair> queue = new ArrayDeque<>();
-        queue.offer(new Pair(row, col));
-        visited[row][col] = true;
-
-        while(!queue.isEmpty()){
-            Pair pair = queue.poll();
-            int i = pair.first;
-            int j = pair.second;
-
-            
-            //Explore the neighbours vertically
-            int k=i-1 <= 0 ? 0 : i-1; 
-            for( ; k<=(i+1) && k<visited.length; k++){
-                if(grid[k][j] == '1' && visited[k][j] ==false){
-                    queue.offer(new Pair(k, j));
-                    visited[k][j] = true;
-                }
+    
+    void bfs(int row, int col, boolean[][] visited, char[][] grid, int m, int n){
+    Queue<Pair> q = new ArrayDeque<>();
+    q.offer(new Pair(row, col));
+    visited[row][col] = true;
+    
+    while(!q.isEmpty()){
+        Pair polPair = q.poll();
+        int prow = polPair.row;
+        int pcol = polPair.col;
+        
+        //visit 4 neigh
+        //1 (0,1)
+        int uprow1 = prow+0 > m-1 ? Integer.MAX_VALUE : prow+0;;
+        int upcol1 = (pcol+1) > n-1 ? Integer.MAX_VALUE : pcol+1;
+        if(uprow1!=Integer.MAX_VALUE && upcol1 !=Integer.MAX_VALUE){
+            if(grid[uprow1][upcol1] == '1' && visited[uprow1][upcol1] == false){
+                q.offer(new Pair(uprow1, upcol1));
+                visited[uprow1][upcol1]=true;
             }
-
-            //Explore the neighbours horizontally
-            int l=j-1 <= 0 ? 0: j-1;
-            for( ; l<=(j+1) && l<visited[0].length; l++){
-                if(grid[i][l] == '1' && visited[i][l] ==false){
-                    queue.offer(new Pair(i, l));
-                    visited[i][l] = true;
-                }
-            }
-
         }
+        
+        //2 (1,0)
+        int uprow2 = prow+1 > m-1 ? Integer.MAX_VALUE : prow+1;;
+        int upcol2 = (pcol+0) > n-1 ? Integer.MAX_VALUE : pcol+0;
+        if(uprow2!=Integer.MAX_VALUE && upcol2 !=Integer.MAX_VALUE){
+            if(grid[uprow2][upcol2] == '1' && visited[uprow2][upcol2] == false){
+                q.offer(new Pair(uprow2, upcol2));
+                visited[uprow2][upcol2]=true;
+            }
+        }
+        
+        //3 (0,-1)
+        int uprow3= prow+0 > m-1 ? Integer.MAX_VALUE : prow+0;;
+        int upcol3 = (pcol-1) < 0 ? Integer.MAX_VALUE : pcol-1;
+        if(uprow3!=Integer.MAX_VALUE && upcol3 !=Integer.MAX_VALUE){
+            if(grid[uprow3][upcol3] == '1' && visited[uprow3][upcol3] == false){
+                q.offer(new Pair(uprow3, upcol3));
+                visited[uprow3][upcol3]=true;
+            }
+        }
+        //4 (-1,0)
+        int uprow4= prow-1 < 0 ? Integer.MAX_VALUE : prow-1;
+        int upcol4 = (pcol+0) > n-1 ? Integer.MAX_VALUE : pcol+0;
+        if(uprow4!=Integer.MAX_VALUE && upcol4 !=Integer.MAX_VALUE){
+            if(grid[uprow4][upcol4] == '1' && visited[uprow4][upcol4] == false){
+                q.offer(new Pair(uprow4, upcol4));
+                visited[uprow4][upcol4]=true;
+            }
+        }
+        
     }
+}
+    
 }
 
 class Pair{
-    int first;
-    int second;
+    int row;
+    int col;
 
-    Pair(int first, int second){
-        this.first = first;
-        this.second = second;
+    Pair(int row, int col){
+        this.row = row;
+        this.col = col;
     }
 }
