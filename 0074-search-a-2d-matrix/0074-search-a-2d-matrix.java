cf1099d15aa977b45a86303f08a894fdd167c9e7
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-01-22 12:07:55 -0500

    Time: 0 ms (100.00%), Space: 42.2 MB (19.51%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,21 +3,19 @@ class Solution {
         int rowlen = matrix.length;
         int collen = matrix[0].length;
         int left = 0;
-        int right = rowlen * collen - 1;
+        int right = (rowlen*collen) - 1;
         
-        while(left <= right){ //0,11   6,11 //8,11
-            int mid = left + (right-left)/2; //5 //8 //9
-            
-            if(matrix[mid/collen][mid%collen]==target){//[1,1] 11 != 30 [2,0] 23!=30 [2,1]=30 30=30
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(matrix[mid/collen][mid%collen]==target){
                 return true;
-            }else if(matrix[mid/collen][mid%collen] > target){ 
-                right = mid-1;    
-            }else if(matrix[mid/collen][mid%collen] < target){ //11<30   23<30
+            }else if(matrix[mid/collen][mid%collen] > target){
+                right = mid-1;
+            }else{
                 left = mid+1;
             }
         }
         return false;
+        
     }
-    //TC O(log m*n)
-    //SC O(1)
 }
