8131be2af819324357dedd6e2104c0f857f92e00
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2024-02-16 13:00:21 -0500

    Time: 0 ms (100.00%), Space: 50.3 MB (56.13%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,31 +1,30 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int low=0;
-        int end=nums.length-2;
-        int idx =  singleelemrec(low, end, nums);
+        int low = 0;
+        int end = nums.length-2;
+        int idx = singleelementindex(low, end, nums);
         return nums[idx];
     }
     
-    int singleelemrec(int low, int end, int[] nums){
-        if(low>end){
+    int singleelementindex(int low, int end, int[] nums){
+        if(low > end){
             return low;
         }
         
-        int mid = (low + end)/2;
+        int mid = (low+end)/2;
         
-        if(mid %2 ==0){
+        if(mid%2==0){
             if(nums[mid] == nums[mid+1]){
-                return singleelemrec(mid+1, end, nums);
+                return singleelementindex(mid+1, end, nums);
             }else{
-                return singleelemrec(low, mid-1, nums);
+                return singleelementindex(low, mid-1, nums);
             }
-        }else if(mid %2 ==1){
+        }else{
             if(nums[mid] == nums[mid-1]){
-                return singleelemrec(mid+1, end, nums);
+                return singleelementindex(mid+1, end, nums);
             }else{
-                return singleelemrec(low, mid-1, nums);
-            }
+                return singleelementindex(low, mid-1, nums);
+            }   
         }
-        return -1;
     }
 }
