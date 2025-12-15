05e5d9871ba7f00cede49678a593c055a4355f35
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2025-12-15 07:40:52 -0500

    Time: 0 ms (100%), Space: 19.1 MB (10.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,12 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        num_ind_map = {}
-
-        for index,num in enumerate(nums):
-            if (target - num) in num_ind_map:
-                return [index, num_ind_map[target - num]]
-            else:
-                num_ind_map[num] = index
-                 
-
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for index, num in enumerate(nums):
+            if (target-num) in seen:
+                return [index, seen.get(target-num)] 
+            seen[num] = index
+        return [-1, -1]
+        
+        #TC - O(N)
+        #SC - O(N)
         
