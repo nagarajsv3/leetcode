0034604134cd8b29e867a9404d8adc293944ae41
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2026-01-11 11:40:29 -0500

    Time: 6 ms (29.51%), Space: 20.7 MB (5.59%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -17,12 +17,11 @@ class Solution(object):
         i = 0
         j = len(numbers)-1
         while(i < j):
-            sum = numbers[i] + numbers[j]
-            if (sum == target):
+            if (numbers[i] + numbers[j] == target):
                 return [i+1, j+1]
-            elif (sum < target):
+            elif (numbers[i] + numbers[j] < target):
                 i = i+1
-            elif (sum > target):
+            elif (numbers[i] + numbers[j] > target):
                 j = j-1
         
         #TC O(n)
