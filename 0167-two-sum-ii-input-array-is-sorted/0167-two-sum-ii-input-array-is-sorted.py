5c850431d1312b3a99acf226ff5f47ad920f427c
Nagaraja Srinivasa Varnekar <30879059+nagarajsv3@users.noreply.github.com>
2026-01-11 11:39:39 -0500

    Time: 7 ms (25.36%), Space: 20.5 MB (5.59%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def twosum_brute(self, numbers, target):
+        arr_len = len(numbers)
+        for i in range(arr_len):
+            for j in range(i+1, arr_len):
+                if (numbers[i] + numbers[j] == target):
+                    return [i+1, j+1]
+
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #return self.twosum_brute(numbers, target)
+        
+        i = 0
+        j = len(numbers)-1
+        while(i < j):
+            sum = numbers[i] + numbers[j]
+            if (sum == target):
+                return [i+1, j+1]
+            elif (sum < target):
+                i = i+1
+            elif (sum > target):
+                j = j-1
+        
+        #TC O(n)
+        #SC O(1)
+            
+
+
+        
